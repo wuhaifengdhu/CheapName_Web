89a1641a0ee5eba788b2fc5829073c8fe326c2cf
Wu Haifeng <hfwu@DMAL-admins-MacBook-Pro.local>
2017-03-13 11:17:15 +0800

    added bootstrap

--- controllers.js
+++ controllers.js
@@ -3,23 +3,35 @@
  */
 'use strict';
 var localStorage = require('./storage'),
-    nameUtil = require('./nameUtil');
+    nameUtil = require('./nameUtil'),
+    queryString = require('querystring');
 
 module.exports = function (app) {
+    var key = "count", count;
+
+    if(localStorage.getItem(key)){
+        count = localStorage.getItem(key);
+    } else {
+        count = 1200;
+    }
     app.get('/', function (req, res) {
         res.render('index', {
-            site_count: 1200
+            site_count: count
         });
     });
 
     app.post('/result', function (req, res) {
-        var user_name = req.body.username;
-        var sex = req.body.sex;
-        var birthday = req.body.birthDay;
-        var dateArray = birthday.split('-');
-        var year = parseInt(dateArray[0]), month = parseInt(dateArray[1]) - 1, day = parseInt(dateArray[2]) - 1;
-
         console.log(req.body);
+        var user_name = req.body.username;
+        var sex = req.body.gender;
+        var birthday = req.body.birthday;
+        var dateArray = birthday.split('/');
+        var year = parseInt(dateArray[2]), month = parseInt(dateArray[0]) - 1, day = parseInt(dateArray[1]) - 1;
+
+
+        count += 1;
+        console.log("count: " + count);
+        localStorage.setItem(key, count);
         res.render('result', {
             name: user_name,
             cheap_name: nameUtil.getName(sex, month, day),
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 var app = express();
 
-var port = process.env.HEALTH_PORT || 6080;
+var port = process.env.HEALTH_PORT || 8080;
 
 app.set('view engine', 'ejs');
 
